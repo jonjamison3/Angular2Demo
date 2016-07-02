7f44e5c93173dce15ed78c34179628d2f6d46d54
jonjamison3 <jonjamison3@gmail.com>
2016-07-02 10:22:20 -0400

    updating to RC4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,9 @@
-import {Component, OnInit } from '@angular/core';
-import {Hero} from './hero';
+import { Component } from '@angular/core';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service'
-
+import {Hero} from './hero';
 @Component({
-  selector:'my-app',
-  template:`
-  <h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `,
-  styles:[`
+  selector: 'my-app',
+  styles: [`
   .selected {
     background-color: #CFD8DC !important;
     color: white;
@@ -64,21 +52,38 @@ import { HeroService } from './hero.service'
     border-radius: 4px 0 0 4px;
   }
 `],
- directives: [HeroDetailComponent],
- providers:[HeroService],
-})
+  template: `
+    <h1>{{title}}</h1>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
-export class AppComponent implements OnInit{
-    constructor(private heroService: HeroService)
-    {}
-    title = 'Tour of Heroes';
-    selectedHero: Hero;
-    onSelect(hero: Hero) { this.selectedHero = hero; };
-    heroes: Hero[];
-    getHeroes(){
-      this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-    };
-    ngOnInit() {
-      this.getHeroes();
-    }
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)"[class.selected]="hero === selectedHero">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    `,
+    directives: [HeroDetailComponent]
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
+  public heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
